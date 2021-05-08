23e22a901ffb8d108aaaf1bb46d92ad304ec97bc
Rachel Moser <moser.rachel@gmail.com>
2021-05-08 15:02:31 -0500

    Add Newsfeed Query Object
    Because:
    * Removes complicated logic from the controller.
    
    This Commit:
    * Created a timeline query object.
    * Tests the timeline query object.

--- app/controllers/news_feeds_controller.rb
+++ app/controllers/news_feeds_controller.rb
@@ -2,12 +2,6 @@
 
 class NewsFeedsController < ApplicationController
   def show
-    posts = Post.where(author_id: current_user.friends).or(Post.where(author_id: current_user))
-                .includes([{ author: [avatar_attachment: :blob] },
-                           { comments: { author: [avatar_attachment: :blob] } }]).sort_by(&:created_at).reverse
-    photos = Photo.where(author_id: current_user.friends).or(Photo.where(author_id: current_user))
-                  .includes([{ author: [avatar_attachment: :blob] },
-                             { comments: { author: [avatar_attachment: :blob] } }]).sort_by(&:created_at).reverse
-    @feed = posts + photos
+    @feed = TimelineQuery.new(current_user).call
   end
 end
---
+++ app/queries/timeline_query.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class TimelineQuery
+  def initialize(user)
+    @user = user
+  end
+
+  def call
+    posts = find_posts
+    photos = find_photos
+    newsfeed = posts + photos
+    newsfeed.sort! { |a, b| -(a.created_at <=> b.created_at) }
+  end
+
+  private
+
+    def find_posts
+      Post.where(author_id: @user.friends).or(Post.where(author_id: @user))
+          .includes([{ author: [avatar_attachment: :blob] }, { comments: { author: [avatar_attachment: :blob] } }])
+          .order(created_at: :desc)
+    end
+
+    def find_photos
+      Photo.where(author_id: @user.friends).or(Photo.where(author_id: @user))
+           .includes([{ author: [avatar_attachment: :blob] }, { comments: { author: [avatar_attachment: :blob] } }])
+           .order(created_at: :desc)
+    end
+end
