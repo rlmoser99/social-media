8ba3807536cde142f247e90506debae3e06074dd
Rachel Moser <moser.rachel@gmail.com>
2021-05-19 19:23:13 -0500

    Feature: Add Pagination for Posts
    Because:
    * There are too many posts for only one page.
    
    This Commit:
    * Adds kaminari gem for pagination.
    * Adds default views to customize.

--- app/controllers/news_feeds_controller.rb
+++ app/controllers/news_feeds_controller.rb
@@ -6,6 +6,6 @@ class NewsFeedsController < ApplicationController
     @feed = Post.where(author_id: @user.friends).or(Post.where(author_id: @user))
                 .includes(postable: { author: [avatar_attachment: :blob],
                                       comments: { author: [avatar_attachment: :blob] } })
-                .order(created_at: :desc)
+                .order(created_at: :desc).page params[:page]
   end
 end
